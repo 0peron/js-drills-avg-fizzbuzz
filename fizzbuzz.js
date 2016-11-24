d1fab65c1dc9680baeee9b4ed4173b652a33509f
0peron <phubbard89@gmail.com>
2016-11-24 01:08:35 -0500

    using if statements

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,22 @@
 function fizzBuzz(countTo) {
-    // your code here
+    //count
+    for (var counter = 1; counter <= countTo; counter++) {
+        var output = counter;
+        if (counter % 3 == 0) {
+            output = "fizz";
+        }
+        if (counter % 5 == 0) {
+            output = "buzz";
+        }
+        if ((counter % 3 == 0) && (counter % 5 == 0)) {
+            output = "fizzbuzz"
+        }
+    }
+    console.log(output);
+
 }
+
+fizzBuzz(16)
 
 
 
